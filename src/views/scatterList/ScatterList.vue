18ce6e950e2c3889699018b979e3f4207be28b3a
bonree <2787329727@qq.com>
2021-11-25 15:47:41 +0800

    图形增加

--- src/views/scatterList/ScatterList.vue
+++ src/views/scatterList/ScatterList.vue
@@ -15,15 +15,10 @@
             @click="dialogTableVisible(item.options)"
           ></div>
         </el-col>
-        <el-col :span="6"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+
       </draggable>
-      <Dialog ref="dialog" :type="type" :title="optionsName" :id="id"/>
+      <Dialog ref="dialog" :type="type" :title="optionsName" :id="id" />
     </div>
   </div>
 </template>
@@ -79,14 +74,125 @@ export default {
             ],
           },
         },
+        {
+          id: "chart14",
+          options: {
+            name: "指数回归",
+            dataset: [
+              {
+                source: [
+                  [1, 4862.4],
+                  [2, 5294.7],
+                  [3, 5934.5],
+                  [4, 7171.0],
+                  [5, 8964.4],
+                  [6, 10202.2],
+                  [7, 11962.5],
+                  [8, 14928.3],
+                  [9, 16909.2],
+                  [10, 18547.9],
+                  [11, 21617.8],
+                  [12, 26638.1],
+                ],
+              },
+              {
+                transform: {
+                  type: "ecStat:regression",
+                  config: {
+                    method: "exponential",
+                    // 'end' by default
+                    // formulaOn: 'start'
+                  },
+                },
+              },
+            ],
+            title: {
+              text: "1981 - 1998 gross domestic product GDP (trillion yuan)",
+              subtext: "By ecStat.regression",
+              sublink: "https://github.com/ecomfe/echarts-stat",
+              left: "center",
+            },
+            tooltip: {
+              trigger: "axis",
+              axisPointer: {
+                type: "cross",
+              },
+            },
+            xAxis: {
+              splitLine: {
+                lineStyle: {
+                  type: "dashed",
+                },
+              },
+            },
+            yAxis: {
+              splitLine: {
+                lineStyle: {
+                  type: "dashed",
+                },
+              },
+            },
+            series: [
+              {
+                name: "scatter",
+                type: "scatter",
+                datasetIndex: 0,
+              },
+              {
+                name: "line",
+                type: "line",
+                smooth: true,
+                datasetIndex: 1,
+                symbolSize: 0.1,
+                symbol: "circle",
+                label: { show: true, fontSize: 16 },
+                labelLayout: { dx: -20 },
+                encode: { label: 2, tooltip: 1 },
+              },
+            ],
+          },
+        },
+        {
+          id: "chart15",
+          options: {
+            name:"基础雷达图",
+            legend: {
+              data: ["Allocated Budget", "Actual Spending"],
+            },
+            radar: {
+              // shape: 'circle',
+              indicator: [
+                { name: "Sales", max: 6500 },
+                { name: "Administration", max: 16000 },
+                { name: "Information Technology", max: 30000 },
+                { name: "Customer Support", max: 38000 },
+                { name: "Development", max: 52000 },
+                { name: "Marketing", max: 25000 },
+              ],
+            },
+            series: [
+              {
+                name: "Budget vs spending",
+                type: "radar",
+                data: [
+                  {
+                    value: [4200, 3000, 20000, 35000, 50000, 18000],
+                    name: "Allocated Budget",
+                  },
+                  {
+                    value: [5000, 14000, 28000, 26000, 42000, 21000],
+                    name: "Actual Spending",
+                  },
+                ],
+              },
+            ],
+          },
+        },
       ],
       optionsName: "",
       type: "scatter",
-      id:"scatterList-charts"
+      id: "scatterList-charts",
     };
-  },
-  computed: {
-  
   },
   mounted() {
     this.draw();
@@ -95,8 +201,14 @@ export default {
     draw() {
       // 基于准备好的dom，初始化echarts实例
       let chart13 = this.$eCharts.init(document.getElementById("chart13"));
+      let chart14 = this.$eCharts.init(document.getElementById("chart14"));
+      let chart15 = this.$eCharts.init(document.getElementById("chart15"));
+      // let chart16 = this.$eCharts.init(document.getElementById("chart16"));
       // 绘制图表
       chart13.setOption(this.list[0].options);
+      chart14.setOption(this.list[1].options);
+      chart15.setOption(this.list[2].options);
+      // chart16.setOption(this.list[3].options);
     },
     dialogTableVisible(options) {
       this.optionsName = options.name;
